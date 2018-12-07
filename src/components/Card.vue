c3248dfb024551c44d9141b69ca565207706ef79
Cory Zibell <cory.zibell@digitalsurgeons.com>
2018-12-07 13:40:49 -0500

    Push up where I’m at

---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+<router-link :to="route">
+  <div class="card">
+    <div class="inside">
+      {{text}}
+    </div>
+  </div>
+</router-link>
+</template>
+
+<script>
+const get = require('simple-get')
+
+export default {
+  name: 'Card',
+  props: {
+    text: String,
+    route: String
+  },
+  // methods: {
+  //   sendRequest(event) {
+
+  //   }
+  // },
+  // computed: {
+  //   upRoute() {
+  //     return this.$route.path
+  //   }
+  // }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  width: 23vw;
+  height: 13vw;
+  position: relative;
+  background: black;
+  clip-path: polygon(0 0, 80% 0%, 100% 20%, 100% 80%, 100% 100%, 20% 100%, 0% 80%, 0% 20%);
+}
+
+.inside {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  right: 2px;
+  bottom: 2px;
+  clip-path: polygon(0 0, 80% 0%, 100% 20%, 100% 80%, 100% 100%, 20% 100%, 0% 80%, 0% 20%);
+  background: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>zion-control</h2>
+    <template v-if="$route.params.environment">
+      <template v-for="(value, key) in $options.config[$route.params.property][$route.params.environment]">
+        <Card :key="key" :text="key" :endpoint="'/'+$route.params.property+'/'+$route.params.environment+'/'+key" />
+      </template>
+    </template>
+    <template v-else-if="$route.params.property">
+      <template v-for="(value, key) in $options.config[$route.params.property]">
+        <Card :key="key" :text="key" :route="'/'+$route.params.property+'/'+$route.params.environment" />
+      </template>
+    </template>
+    <template v-else>
+      <template v-for="(value, key) in $options.config">
+        <Card :key="key" :text="key" :route="'/'+$route.params.property" />
+      </template>
+    </template>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
+const config = require('@/../config.js')
 
 export default {
   name: 'home',
+  config: config,
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
